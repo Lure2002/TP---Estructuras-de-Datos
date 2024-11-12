aa8855e7348b2a6887ba1455812e6935f31e0da4
Lucas Contreras <lucastec2002@hotmail.com>
2024-11-12 14:02:11 -0300

    clase base paciente

--- clases/paciente.py
+++ clases/paciente.py
@@ -5,3 +5,16 @@ class Paciente:
         self.edad = edad
         self.historial_enfermedades = historial_enfermedades or []
         self.medicamentos = medicamentos or []
+
+    def agregar_enfermedad(self, enfermedad):
+        self.historial_enfermedades.append(enfermedad)
+
+    def agregar_medicamento(self, medicamento):
+        self.medicamentos.append(medicamento)
+
+    def eliminar_enfermedad(self, enfermedad):
+        if enfermedad in self.historial_enfermedades:
+            self.historial_enfermedades.remove(enfermedad)
+
+    def __str__(self):
+        return f"Paciente({self.id_paciente}): {self.nombre}, Edad: {self.edad}, Enfermedades: {self.historial_enfermedades}, Medicamentos: {self.medicamentos}"
